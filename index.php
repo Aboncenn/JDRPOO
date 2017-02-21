6c6564a5dae0065bffd60536860e22c01099b5b6
Paul JULIEN <julien.paul95@gmail.com>
2017-02-21 09:25:08 +0100

    ajout bootstrap pour le kiff

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
 
 <?php
 require 'classes/Personnage.php';
 
 
-    $paladin = new Paladin('Paladin');
-    $guerrier = new Guerrier('Guerrier');
-    $magicien = new Magicien('Magicien');
+$paladin = new Paladin('Paladin');
+$guerrier = new Guerrier('Guerrier');
+$magicien = new Magicien('Magicien');
 
-?> <p> Quel personnage veux tu choisir ?</p>
+?>
+
+<strong> ZONE TEST VARIABLES / CLASSES</strong>
+
+<p> Quel personnage veux tu choisir ?</p>
 <form method="post">
     <select name="personnage" id="personnage">
         <option value="paladin">  <?php echo $paladin->nom; ?> <br></option>
@@ -21,3 +45,15 @@ require 'classes/Personnage.php';
 
 <?php
 var_dump($guerrier, $magicien, $paladin);
+
+?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
